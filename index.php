54e086ce49e6a94a5e853b168ef184d72e1ed29c
joecohen <joecohen@cs.umb.edu>
2015-02-23 14:58:32 -0500

    fixed back button

--- index.php
+++ index.php
@@ -339,7 +339,7 @@ $(function(){
 	    	<label class="control-label col-sm-2" for="email">Desired Username:</label>
 	    <div class="col-sm-10">
 	    	<div class="input-group">
-		        <input  type="username" autocomplete="off" class="form-control" id="username" name="username" placeholder="Enter username here. It will become the email username@cs.umb.edu.">
+		        <input  type="username" class="form-control" id="username" name="username" placeholder="Enter username here. It will become the email username@cs.umb.edu.">
 		    	<span id="usernameerrortext" class="input-group-addon"></span>
 	    	</div>
 	    </div>
@@ -349,14 +349,14 @@ $(function(){
 	<div class="form-group">
 	    <label class="control-label col-sm-2" for="pwdraw">Password:</label>
 	    <div class="col-sm-10"> 
-	      <input type="password" autocomplete="off" class="form-control" id="passwordraw" name="passwordraw" placeholder="Enter password">
+	      <input type="password" class="form-control" id="passwordraw" name="passwordraw" placeholder="Enter password">
 	    </div>
 	</div>
 	
 	<div class="form-group">
 	    <label class="control-label col-sm-2" for="pwd">Password Hash:</label>
 	    <div class="col-sm-10"> 
-	      <input readonly=true autocomplete="off" class="form-control" id="password" name="password" placeholder="Please enable JavaScript">
+	      <input readonly=true  class="form-control" id="password" name="password" placeholder="Please enable JavaScript">
 	    </div>
 	</div>
 	
